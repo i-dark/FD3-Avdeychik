1231084dc7019b5fb2bbd4c00090a49d0b5d9303
Vladislav <vladislav.avdeychik@rombit.be>
2018-09-17 00:14:57 +0300

    Ishop Initial commit

---
+++ ishop/ItemBlock.js
@@ -0,0 +1,37 @@
+﻿var ItemBlock = React.createClass({
+
+        displayName: 'ItemBlock',
+
+        propTypes: {
+            item: React.PropTypes.shape({
+                id: React.PropTypes.number.isRequired,
+                name: React.PropTypes.string.isRequired,
+                url: React.PropTypes.string,
+                leftInStock: React.PropTypes.number.isRequired,
+            })
+
+        },
+
+        render: function () {
+
+            const url = this.props.item.url ? this.props.item.url : "http://up100.uppolice.gov.in/PublishingImages/No-Image-Basic.png"
+            const img = <img src={url} />;
+
+            return (
+                <tr>
+                    <td>
+                        <div className="imgBlock">
+                            {img}
+                        </div>
+                    </td>
+                    <td>
+                        {this.props.item.name}
+                    </td>
+
+                </tr>
+            );
+        }
+        ,
+
+    })
+;
---
+++ ishop/ShopBlock.js
@@ -0,0 +1,31 @@
+﻿var ShopBlock = React.createClass({
+
+    displayName: 'ShopBlock',
+
+    propTypes: {
+        catalog: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                id: React.PropTypes.number.isRequired,
+                name: React.PropTypes.string.isRequired,
+                url: React.PropTypes.string,
+                leftInStock: React.PropTypes.number.isRequired,
+            })
+        )
+    },
+
+    render: function () {
+
+        const listItems = this.props.catalog.map((item) =>
+            <ItemBlock key={item.id} item={item}></ItemBlock>
+        );
+
+        return (<div className="ShopBlock">
+            <table className="catalog" width="100%">
+                <tbody>
+                {listItems}
+                </tbody>
+            </table>
+        </div>);
+    },
+
+});
