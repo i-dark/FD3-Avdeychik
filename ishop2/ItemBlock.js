bca0fa7fa4ffc581aa4d3680eb0072d484a1a621
Vladislav <vladislav.avdeychik@rombit.be>
2018-09-23 18:22:24 +0300

    iShop 2

---
+++ ishop2/ItemBlock.js
@@ -0,0 +1,59 @@
+﻿var ItemBlock = React.createClass({
+
+        displayName: 'ItemBlock',
+
+        propTypes: {
+            item: React.PropTypes.shape({
+                id: React.PropTypes.number.isRequired,
+                name: React.PropTypes.string.isRequired,
+                price: React.PropTypes.number.isRequired,
+                url: React.PropTypes.string,
+                leftInStock: React.PropTypes.number.isRequired,
+            }),
+        },
+
+        getInitialState: function () {
+            return {
+                'selected' : false
+            };
+        },
+
+
+        deleteItem: function (event) {
+            this.props.deleteCallback(this.props.item.id);
+        },
+
+        selectItem: function (event) {
+            this.setState({selected: !this.state.selected});
+        },
+
+        render: function () {
+
+            const url = this.props.item.url ? this.props.item.url : "http://up100.uppolice.gov.in/PublishingImages/No-Image-Basic.png"
+            const img = <img src={url} />;
+
+            return (
+                <tr onClick={this.selectItem}
+                    className={this.state.selected ? "SelectedItem" : ""}>
+                    <td>
+                        <div className="imgBlock">
+                            {img}
+                        </div>
+                    </td>
+                    <td>
+                        {this.props.item.name}
+                    </td>
+                    <td>
+                        {this.props.item.price} $
+                    </td>
+                    <td>
+                        <input value={"delete"} type={"button"} onClick={this.deleteItem}/>
+                    </td>
+
+                </tr>
+            );
+        }
+        ,
+
+    })
+;
---
+++ ishop2/ShopBlock.js
@@ -0,0 +1,44 @@
+﻿var ShopBlock = React.createClass({
+
+    displayName: 'ShopBlock',
+
+    propTypes: {
+        catalog: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                id: React.PropTypes.number.isRequired,
+                name: React.PropTypes.string.isRequired,
+                price: React.PropTypes.number.isRequired,
+                url: React.PropTypes.string,
+                leftInStock: React.PropTypes.number.isRequired,
+            })
+        )
+    },
+
+    getInitialState: function () {
+        return {
+            'catalog': this.props.catalog
+        };
+    },
+
+    deleteItemCallback: function (id) {
+        this.setState(
+            {catalog: this.state.catalog.filter(item => item.id != id)}
+        );
+    },
+
+    render: function () {
+
+        const listItems = this.state.catalog.map((item) =>
+            <ItemBlock key={item.id} item={item} deleteCallback={this.deleteItemCallback}/>
+        );
+
+        return (<div className="ShopBlock">
+            <table className="catalog" width="100%">
+                <tbody>
+                {listItems}
+                </tbody>
+            </table>
+        </div>);
+    },
+
+});
