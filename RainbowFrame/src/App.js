6a7429ecb537984983492e35a698eb3756be4a64
Vladislav <vladislav.avdeychik@rombit.be>
2018-10-06 23:35:03 +0300

    Rainbow

---
+++ RainbowFrame/src/App.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Rainbow from "./components/Rainbow/Rainbow.js"
+
+// let colors = ["#aabbcc", "#F00", "#0FF", "#00F","rgb(255,0,0)"];
+
+let colors = [];
+
+let r = 50;
+let g = 0;
+let b = 50;
+for (let i=0; i<255; i++){
+    r = r < 255 ? r+1 : r;
+    g = g < 255 ? g+1 : g;
+    b = b < 255 ? b+2 : b;
+    colors.unshift(`rgb(${r},${g},${b})`);
+}
+
+var shopCatalog = require("./catalog");
+
+    ReactDOM.render(
+        <Rainbow colors={colors} level={5}>
+            Some Text
+        </Rainbow>
+    , document.getElementById('container')
+);
+
---
+++ RainbowFrame/src/components/Rainbow/Rainbow.js
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+class Rainbow extends React.Component {
+
+    render() {
+        let color = this.props.colors.pop();
+
+        return (<div style={{border: "1px solid " + color}}>
+            {
+                this.props.colors.length>0 && <Rainbow colors={this.props.colors}>{this.props.children}</Rainbow>
+            }
+            {
+                this.props.colors.length===0 && this.props.children
+            }
+        </div>);
+    }
+
+}
+
+export default Rainbow;
